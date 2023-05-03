d46e1141ce0adce152f056988bb58fc5b9e11074
Pierre <p.esseul@gmail.com>
2023-05-03 15:14:56 +0200

    amelioration contact and home page

--- src/Components/Contact/Contact.tsx
+++ src/Components/Contact/Contact.tsx
@@ -2,17 +2,19 @@ import './Contact.css'
 
 function Contact() {
     return (
-        <>
+        <div className='contact'>
             <div>
                 <p>p.esseul@gmail.com</p>
 
-                <a href="https://www.linkedin.com/in/pierre-esseul-1a3443249/"><img src="/img/socialNetwork/in.png" alt="logo linkedin" /></a>
-                <a href="https://github.com/PierreEsseul"><img src="/img/socialNetwork/github.png" alt="logo github" /></a>
+                <div className='network'>
+                    <a href="https://www.linkedin.com/in/pierre-esseul-1a3443249/"><img src="/img/socialNetwork/in.png" alt="logo linkedin" className='logo' /></a>
+                    <a href="https://github.com/PierreEsseul"><img src="/img/socialNetwork/github.png" alt="logo github" className='logo' /></a>
+                </div>
             </div>
             <div>
-                <img className="profil" src="/img/photo_profil.jpeg" alt="profile Pierre ESSEUL" />
+                <img className="profil" src="/img/photo_profil.jpg" alt="profile Pierre ESSEUL" />
             </div>
-        </>    )
+        </div>    )
 }
 
 export default Contact;
